bdc6af9587a6e35e73df7095a2be532250333def
Kiran <sainikiran7852@gmail.com>
2026-06-11 13:53:39 +0530

    completed day 7:UI integration and API verification

--- server/src/index.js
+++ server/src/index.js
@@ -20,7 +20,7 @@ const documentRoutes = require('./routes/documentRoutes');
 // Mount the routes onto your active express application instance
 app.use('/api/docs', documentRoutes);
 
-const router = express.Router();
+//const router = express.Router();
 
 const signatureRoutes = require('./routes/signatureRoutes'); 
 app.use('/api/signatures', signatureRoutes); 
